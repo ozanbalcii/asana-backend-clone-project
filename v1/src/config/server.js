dabf1e7b3b7dc1fec72c7d4a1f5a416f08f38f89
ozan <ozanbalcieem@gmail.com>
2022-11-03 01:37:43 +0300

    Create server.js

---
+++ v1/src/config/server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv'); // .env dosyasını tanımladık.
+
+
+
+
+
+module.exports = () => {  //tanımladıgımız şeyi fonksiyon olarak kullanabilmemiz için, fonksiyon olarak yazdık bunu
+    dotenv.config();
+};
+
