183baeba41444bb7f727f03d6d43ffabd114038c
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2022-11-03 12:43:09 +0300

    Update db.js

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -1,23 +1,17 @@
-//*BU DOSYADA MONGOOSE İLE MONGO DB'YE BAĞLANACAĞIZ.
-
 const Mongoose = require('mongoose');
+const db = Mongoose.connection; 
 
-const db = Mongoose.connection; //connection'ı aldık.
 
-
-db.once("open", ( ) => { //db bağlantısı gerçekleşirse:
+db.once("open", ( ) => { 
     console.log("DB Connection is currently successful.");    
 });
 
 const connectDB = async  ( ) => {
-    //burası bizim belirediğimiz yola bağlantı sağlar. Bu bağlantı ile db connection'a sahip olur.
-    // burada beklemesini istediğim ve alt satıra gitmemesini istediğim için await koydum.
 await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{ 
-    useNewUrlParser : true,  //bu iki kodun anlamına bak, bunlara bakın dedi.
+    useNewUrlParser : true,  
     useUnifiedTopology : true,
 });
 };
-
 
 module.exports =  {
     connectDB
