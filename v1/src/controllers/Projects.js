abf4b7c9b0714e43e6a1e7f1745841fbcfceba7e
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2022-11-03 12:44:52 +0300

    Update Projects.js

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,47 +1,23 @@
-//logical işlemler(matıksal) burada gerçekleşiyor
-
-
 const {insert, list} = require ("../services/Projects");
 const httpStatus = require("http-status");
 
-// const create = (req,res) => {
-//     insert({ name: "test project"})
-//     .then(response =>{
-//         res.status(httpStatus.CREATED).send(response);
-//     })
-//     .catch((e) => {
-//         res.status(httpStatus.INTERNAL_SERVER_ERRO).send("Project created");
-//     })
- 
-// };
-
-
-// const index = (req,res) => {
-   
-//     res.status(200).send("Project created");
-// };
-
 
 const create = (req, res) => {
-    insert(req.body)        //*req.body'den name alcaz yani: postmanden(front end) name vercez.(insert öylesine verilmiş bir isimdir.)
-    .then((response) =>{     // dogruysa then ile response(cevap) döndü.
+    insert(req.body)        
+    .then((response) =>{    
         res
-        .status(httpStatus.CREATED)     //http status npm paketine ait özellik : httpStatus.CREATED BİZİM İÇİN EĞER DÜZGÜN ÇALIŞTIYSA BURADA STATUS : 200
+        .status(httpStatus.CREATED)     
         .send(response);
     })
-    .catch((e) => { // ynls ise catch'le yakala
+    .catch((e) => { 
         res
-        .status(httpStatus.INTERNAL_SERVER_ERROR) // HATA VARSA KENDİSİ ERROR STATUS SAYISINI DÖNDÜRÜYOR PAKET SAYESİNDE
+        .status(httpStatus.INTERNAL_SERVER_ERROR) 
         .send(e);
     });
 };
 
-
-// index'in amacı: bütün projenin response'unu alabilmemize yarıyor.
-// altta da yorumda yazdım response'ye sonucları attık, response sayesinde sonucları döndürebiliyoruz index'te
-const index = (req, res) => { // get requestliyoruz ve bize kaydedilen userları veriyor.
-
-    list()  //servise'de kayıt ediyorduk ve altta list olusturduk. burada sonuçları find ile döndürdük. burada response alıyorduk, response=cevap oldugu için ise repsonse'de sonuçlar döndü.
+const index = (req, res) => { 
+    list()  
         .then(( response ) => {
         res
         .status(httpStatus.OK)
@@ -57,14 +33,3 @@ module.exports = {
     create,
     index
 };
-
-
-
-
-
-
-
-
-
-
-
