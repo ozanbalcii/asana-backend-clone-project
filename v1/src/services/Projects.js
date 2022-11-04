a10885a03ac93b45807fdeeeec427da28738acb5
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2022-11-04 14:42:33 +0300

    Update Projects.js

--- v1/src/services/Projects.js
+++ v1/src/services/Projects.js
@@ -1,19 +1,13 @@
-//burası modelde oluşturulan şeylerin kayıt işlemini gerçekleştirmek ve hata varsa yönlendirme yapmak içindir.
-//*services altındakinleri  controllers'da kullanıyoruz.
+const Project = require("../models/Projects");
 
-const Project = require("../models/Projects");// user modeli alcaz cunku burada kaydedilecek mongoya
-
-
-// kayıt işlemi gerçekleştirilecek:
-const insert = (data) => {
+const insert = (data) => { // register
     const project = new Project(data); 
-    return project.save();  //*save'i return etmezsek, bir hata vb durumunu veya işlem dogru mu yapılıyor bilemeyiz.
-};                           // save'i return edersek hataları yakalayabileceğimiz hale gelir
-                             // hataları, insert nerde(controllers>project.js) çalışıtırıyorsam creat içinde kontrol edeceğim
-                             // hatta catch ile yakalıyoruz hataları
-
+    return project.save();  
+};                           
+                             
+                            
 const list = () => {
-    return Project.find({});  // boş find({}) yaparsak sonucları donuduruyor, yazarsak buluyor onu ve onu dönüdürüyor.
+    return Project.find({});  
 }
 
 
