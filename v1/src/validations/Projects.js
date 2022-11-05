5ae9f46d53ba06454701423fdd0a74588f05984f
ozan <ozanbalcieem@gmail.com>
2022-11-05 12:31:41 +0300

    Create Projects.js

---
+++ v1/src/validations/Projects.js
@@ -0,0 +1,36 @@
+const Joi = require ("joi"); //validation(doğrulama) için joi package'ını kullanıyoruz.
+
+
+//*BURADA SCHEMA YAPTIK:çümkü joi dokumanına baktıgımızda böyle bir yapısı vardı.
+// name'in özelliklerini girdik, örneğin string, required ve min:5 olması gibi
+// bunlar validation'u sağlatan şeylerdir. Yani bu koşullar sağlanarak doğrulama yapılıyor.
+
+const createValidation = Joi.object({ //joi.object : joi'de obje üret
+    name: Joi
+    .string()
+    .required()
+    .min(5)
+});
+
+//* validation yaparken, bir işlem geldiğinde araya girip doğrulama yapması gerekmektedir.
+// bunun için middileware kullancağız.
+
+module.exports = {  // objede exports'ladık çünkü 1 den fazla validation yapılacak.
+    createValidation,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
