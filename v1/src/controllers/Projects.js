5691d15b3abed3be5b6eb7a22de60dc7aa67e3f2
ozan <ozanbalcieem@gmail.com>
2022-11-05 12:31:19 +0300

    Update Projects.js

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,23 +1,29 @@
+//logical işlemler(matıksal) burada gerçekleşiyor
+
 const {insert, list} = require ("../services/Projects");
 const httpStatus = require("http-status");
 
 
 const create = (req, res) => {
-    insert(req.body)        
-    .then((response) =>{    
+    insert(req.body)        //*req.body'den name alcaz yani: postmanden(front end) name vercez.(insert öylesine verilmiş bir isimdir.)
+    .then((response) =>{     // dogruysa then ile response(cevap) döndü.
         res
-        .status(httpStatus.CREATED)     
+        .status(httpStatus.CREATED)     //http status npm paketine ait özellik : httpStatus.CREATED BİZİM İÇİN EĞER DÜZGÜN ÇALIŞTIYSA BURADA STATUS : 200
         .send(response);
     })
-    .catch((e) => { 
+    .catch((e) => { // ynls ise catch'le yakala
         res
-        .status(httpStatus.INTERNAL_SERVER_ERROR) 
+        .status(httpStatus.INTERNAL_SERVER_ERROR) // HATA VARSA KENDİSİ ERROR STATUS SAYISINI DÖNDÜRÜYOR PAKET SAYESİNDE
         .send(e);
     });
 };
 
-const index = (req, res) => { 
-    list()  
+
+// index'in amacı: bütün projenin response'unu alabilmemize yarıyor.
+// altta da yorumda yazdım response'ye sonucları attık, response sayesinde sonucları döndürebiliyoruz index'te
+const index = (req, res) => { // get requestliyoruz ve bize kaydedilen userları veriyor.
+
+    list()  //servise'de kayıt ediyorduk ve altta list olusturduk. burada sonuçları find ile döndürdük. burada response alıyorduk, response=cevap oldugu için ise repsonse'de sonuçlar döndü.
         .then(( response ) => {
         res
         .status(httpStatus.OK)
@@ -33,3 +39,14 @@ module.exports = {
     create,
     index
 };
+
+
+
+
+
+
+
+
+
+
+
