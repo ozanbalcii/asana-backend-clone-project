96cac336f13880ce2cd67ee2925cfcb0e60bb13b
ozan <ozanbalcieem@gmail.com>
2022-11-05 12:31:16 +0300

    Update server.js

--- v1/src/config/server.js
+++ v1/src/config/server.js
@@ -1,6 +1,10 @@
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv'); // .env dosyasını tanımladık.
 
 
-module.exports = () => {  
+
+
+
+module.exports = () => {  //tanımladıgımız şeyi fonksiyon olarak kullanabilmemiz için, fonksiyon olarak yazdık bunu
     dotenv.config();
 };
+
