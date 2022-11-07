e46c78a01e423267fe0003eaf60e2a02d53e22db
ozan <ozanbalcieem@gmail.com>
2022-11-07 09:20:47 +0300

    Update Projects.js

--- v1/src/models/Projects.js
+++ v1/src/models/Projects.js
@@ -1,8 +1,10 @@
+//MODELS DOSYASINDA VERİ TABANINDAKİ OBJELERİN TANIMLARI BULUNACAKTIR.
 const Mongoose = require('mongoose');
 const logger = require("../scripts/logger/Projects"); // log basma işlemini burda yapcaz, ondan ekledik.
 
 
-const ProjectSchema = new Mongoose.Schema( 
+//mongoose modelini olusturmamız için Schema Modellerimizi olusturuyoruz:
+const ProjectSchema = new Mongoose.Schema( // mongoose'a schema oluşturttuk.
 {
     name: String,
     // user_id : {
@@ -10,7 +12,7 @@ const ProjectSchema = new Mongoose.Schema(
     //     ref : "user"
     // }
     },
-    {timestamps: true, versionKey: false}
+    {timestamps: true, versionKey: false} //mongoda görmek istediğimiz ve istemediğimiz şeyleri ayarladık.
 );
 
 //* schema burda oldugu için hook'u burada yazdık. ve bu dokumana gore yazdık: https://mongoosejs.com/docs/middleware.html#defining
@@ -22,4 +24,12 @@ ProjectSchema.post("save" ,(doc)=>{      //kayıt sonrasında hook yardımı ile
 });
 
 
+
 module.exports = Mongoose.model("project", ProjectSchema);
+
+
+
+
+
+
+
