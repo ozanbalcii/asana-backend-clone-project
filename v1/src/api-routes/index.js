e4972ef5aacc6c74e2e85ee827f83cf781d91214
ozan <ozanbalcieem@gmail.com>
2022-11-08 13:26:00 +0300

    Update index.js

--- v1/src/api-routes/index.js
+++ v1/src/api-routes/index.js
@@ -1,1 +1,2 @@
 module.exports.ProjectRoutes =  require('./Projects'); // ProjectRoutes object'tir. (bu dosyadaki (api-routes'daki) ./projects i aldık)
+module.exports.UserRoutes =  require('./Users'); 
