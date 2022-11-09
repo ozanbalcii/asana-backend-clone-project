7e8311e45aed5018987514e517725052a4166775
ozan <ozanbalcieem@gmail.com>
2022-11-09 11:28:08 +0300

    Create authenticate.js

---
+++ v1/src/middlewares/authenticate.js
@@ -0,0 +1,26 @@
+// token middlware'si:
+
+const httpStatus = require("http-status");
+const JWT = require("jsonwebtoken");
+
+// token'nın doğrulamasını yapıyoruz:
+const authenticateToken = (req, res, next) => {
+
+    // authorization adı altında bu bilgiyi yollucaz
+    // req.headers.authorization  bunu da kullanabilirdik.
+    // const authHeader = req.headers["authorization"]; // authorization'u header'dan alıyoruz.
+    // const token = authHeader && authHeader.split(" ")[1];   //accesstoken'I alıyoruz split ile
+    const token = req.headers?.authorization?.split(" ")[1] || null ; // hemen üst satırdakinin alternatifi
+if(token == null){ // token yoksa, gönderilmez ise..
+ return res.status(httpStatus.UNAUTHORIZED).send({ error: "bu islemi yapmak için ilk olarak giris yapmalısınız."});
+}
+
+// token varsa verify olur:
+JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => { 
+ if(err) return res.status(httpStatus.FORBIDDEN).send({ error: err}); // token süresi geçerse if'e takılır.
+ req.user = user;  // token süresi geçmemişse requeste user'ı ekleriz. request üstünden gerçekleştiği yaptık ve user eklememizin sebebi user'la ilgili bişey yapabilelim diyedir.
+ next();
+});
+};
+
+module.exports = authenticateToken;
