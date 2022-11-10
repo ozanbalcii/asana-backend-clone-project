668653c38611bb821af028299f93f9ff697479ef
ozan <ozanbalcieem@gmail.com>
2022-11-10 12:40:59 +0300

    Update Projects.js

--- v1/src/api-routes/Projects.js
+++ v1/src/api-routes/Projects.js
@@ -1,5 +1,6 @@
 
 const validate= require('../middlewares/validate');
+const authenticate = require('../middlewares/authenticate');
 // //*validate'in içinde schema olması lazım, onun için schmeas olusturuldu. ALTTA DA VALİDATE'E SCHEMA'YI EKLİYORUZ.
 const schemas = require("../validations/Projects");
 const express = require('express');
@@ -10,13 +11,13 @@ const router = express.Router(); // express'in router'ını kullanıyoruz.
 
 
 // get request gelince create çalışıyor ardından creat'in konumu /controllers/Projects old için buraya gidiyor.
- router.get("/", index); //mvp mimarisinden şaşmamak için ve gereksiz kod kirliliğinden kaçınmak için bu kodun belli ksımını create'de yazdım. ve get olunca create çalışıyor.
-
+ router.route("/").get(authenticate, index);  
+  
  router
  .route("/")
 //validate'e schema lazım olan validate'nin kendi schema'sı = schemas.createValidation burada.
 //çünkü orada string olması, min:5 harf olması gibi koşullar var, onları validate ederse çalışacak.
- .post(validate(schemas.createValidation), create);  // "/" gelince create'den once validate çalışacak.
+ .post(authenticate, validate(schemas.createValidation), create);  // "/" gelince create'den once validate çalışacak.
 
 
 
