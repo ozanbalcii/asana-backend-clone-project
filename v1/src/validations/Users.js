c790cd7a77197113da0f1794aadb6c8b1b7ff046
ozan <ozanbalcieem@gmail.com>
2022-11-11 14:38:11 +0300

    Create Users.js

---
+++ v1/src/validations/Users.js
@@ -0,0 +1,23 @@
+const Joi = require ("joi"); //validation(doğrulama) için joi package'ını kullanıyoruz.
+
+
+
+const createValidation = Joi.object({ //joi.object : joi'de obje üret
+    full_name: Joi.string().required().min(3),
+    password: Joi.string().required().min(8),
+    email: Joi.string().email().required().min(8),
+});
+
+const loginValidation = Joi.object({ 
+    password: Joi.string().required().min(8),
+    email: Joi.string().email().required().min(8),
+});
+
+
+//* validation yaparken, bir işlem geldiğinde araya girip doğrulama yapması gerekmektedir.
+// bunun için middileware kullancağız.
+
+module.exports = {  // objede exports'ladık çünkü 1 den fazla validation yapılacak.
+    createValidation,
+    loginValidation,
+}
