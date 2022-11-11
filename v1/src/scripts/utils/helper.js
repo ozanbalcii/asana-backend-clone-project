c42d9e97cca98f23309e48833d3c7265d8b1ad30
ozan <ozanbalcieem@gmail.com>
2022-11-11 14:38:05 +0300

    Update helper.js

--- v1/src/scripts/utils/helper.js
+++ v1/src/scripts/utils/helper.js
@@ -1,8 +1,10 @@
+// token işlemleri ve password haslama işlemleri yapılacak.
 // password'u hash'leme işlemini bir çok yerde kullanabiliriz, bunun için burada hashlama yapacağız.
 // password hash'i Controllers users'da create'da kullancaz, çünkü create olmadan önce şifreyi gizlicez.
 
-
 const CryptoJS = require('crypto-js');
+const JWT = require("jsonwebtoken");
+
 
 const passwordToHash = (password) => { // password'u çektik
     //iç içe çift şifreleme yaparak güvenliği arttıracağız.
@@ -11,8 +13,20 @@ const passwordToHash = (password) => { // password'u çektik
     return CryptoJS.HmacSHA256(password, CryptoJS.HmacSHA1(password, process.env.PASSWORD_HASH).toString()).toString();
 };
 
-module.exports = {
-    passwordToHash,
+
+
+// token process:(token, şifre ile alakalı oldugu için burada yapıyoruz.)
+// generateAccessToken  ve  generateRefreshToken ları user'a göre üretiyoruz(bundan dolayı parametre user girildi) 
+const generateAccessToken = (user) => {
+    //{name: user.email, ...user} --> email bilgisin aldık, userın kendisini aldık
+    return JWT.sign({name: user.email, ...user}, process.env.ACCESS_TOKEN_SECRET_KEY, {expiresIn: "1w"});
+};
+const generateRefreshToken = (user) => {
+    return  JWT.sign({name: user.email, ...user}, process.env.REFRESH_TOKEN_SECRET_KEY);
 };
 
-
+module.exports = {
+    passwordToHash,
+    generateAccessToken,
+    generateRefreshToken,
+};
