54acb7c3198204c541ef2f5b716c7e741ce7487b
ozan <ozanbalcieem@gmail.com>
2022-11-12 09:18:22 +0300

    Update Projects.js

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,10 +1,11 @@
 //logical işlemler(matıksal) controllers'da gerçekleşiyor
 
-const {insert, list} = require ("../services/Projects");
+const {insert, modify, list} = require ("../services/Projects");
 const httpStatus = require("http-status");
 
 
 const create = (req, res) => {
+    req.body.user_id = req.user;// models user_id'nin tpye ve ref'ini biliyor. Bundan dolayı bir obje yollandıgında models'daki tanımlamalar sayesinde user_id'yi alacak ve user_name olarak kullanacak. Yani postmande çıkışta user_name gözükmeyecek user_id gözükecek. 
     insert(req.body)        //*req.body'den name alcaz yani: postmanden(front end) name vercez.(insert öylesine verilmiş bir isimdir.)
     .then((response) =>{     // dogruysa then ile response(cevap) döndü.
         res
@@ -22,7 +23,6 @@ const create = (req, res) => {
 // index'in amacı: bütün projenin response'unu alabilmemize yarıyor.
 // altta da yorumda yazdım response'ye sonucları attık, response sayesinde sonucları döndürebiliyoruz index'te
 const index = (req, res) => { // get requestliyoruz ve bize kaydedilen userları veriyor.
-
     list()  //servise'de kayıt ediyorduk ve altta list olusturduk. burada sonuçları find ile döndürdük. burada response alıyorduk, response=cevap oldugu için ise repsonse'de sonuçlar döndü.
         .then(( response ) => {
         res
@@ -33,20 +33,23 @@ const index = (req, res) => { // get requestliyoruz ve bize kaydedilen userları
         .status(httpStatus.INTERNAL_SERVER_ERROR)
         .send(e));   
     };
+
+const update = (req, res) => { 
+  if(!req.params?.id){
+    //*req'deki params'ın içinde id yoksa.. (return demezsek kullanamz ve asagı devam eder. RETURN DEDİĞİMİZ İÇİN ELSE KOYMAMIZA GEREK YOKTUR.)
+    return res.status(httpStatus.BAD_REQUEST).send({ 
+    message: "ID information missing"
+  });
+}
+// id var ise: update yap
+modify(req.body, req.params.id).then(updatedProject => {  // req.body ve id= req.params.id yolluyoruz.
+    res.status(httpStatus.OK).send(updatedProject)
+}).catch(e => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: "has a problem while recording"}))
+};
   
 
 module.exports = {
     create,
-    index
+    index,
+    update,
 };
-
-
-
-
-
-
-
-
-
-
-
