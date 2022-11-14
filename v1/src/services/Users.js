c5c8d89665f40ceea0fc150e0697cd8fea1ead54
ozan <ozanbalcieem@gmail.com>
2022-11-14 12:00:43 +0300

    Update Users.js

--- v1/src/services/Users.js
+++ v1/src/services/Users.js
@@ -1,8 +1,8 @@
-const { x } = require("joi");
+
 const User = require("../models/Users");// Models dosyasından user modeli alcaz cunku burada kaydedilecek mongoya
 
 
-// kayıt işlemi gerçekleştirilecek:
+// kayıt işlemi gerçekleştirilecek:   
 const insert = (data) => {
     const user = new User(data); 
     return user.save();  
@@ -12,7 +12,7 @@ const insert = (data) => {
 // services'da data'ya kaydediyorduk, ondan burada bilgiler var ve buradan logindata bilgisini alıyoruz ve loginUser'a koyuyoruz ve controllers'da kullanıyoruz.
 const loginUser = ( loginData ) => {
     return User.findOne(loginData); //* içindekine göre sorgu yaptırtıyor.(loginData: benim gönderdiğim data'dır)
-}
+};
 
 const list = () => { // listeye datadaki bilgileri atcaz (find ile bulcaz ve atcaz)
     return User.find({});  
