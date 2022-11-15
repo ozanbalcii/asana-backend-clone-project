ca294a665e5af1085db1c123c2de27d2ae76fec2
ozan <ozanbalcieem@gmail.com>
2022-11-15 10:04:25 +0300

    Update Users.js

--- v1/src/api-routes/Users.js
+++ v1/src/api-routes/Users.js
@@ -1,32 +1,21 @@
 
 const validate= require('../middlewares/validate');     //validate middleware
 const schemas = require("../validations/Users");       // validations
+const authenticate = require('../middlewares/authenticate');
 const express = require('express');
-const { create, index, login, projectList } =  require("../controllers/Users"); 
+const { create, index, login, projectList, resetPassword } =  require("../controllers/Users"); 
 
 const router = express.Router(); 
 
 
-
- 
  router.get("/", index); 
  router.route("/").post(validate(schemas.createValidation), create);  
  router.route("/login").post(validate(schemas.loginValidation), login);  
- router.route("/projects").get(projectList);  
-
-
-
-
-
-module.exports = router; 
+ router.route("/projects").get(authenticate, projectList);  // authenticate'i yapmazsak request'in içindeki user'I set set etmez ve çalışmaz
+ router.route("/reset-password").post(validate(schemas.resetPasswordValidation), resetPassword);
 
 
 
  
 
-
-
-
-
-
-
+module.exports = router; 
