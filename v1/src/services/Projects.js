18e429de11b59e47b95607d0aaa96db4d190135e
ozan <ozanbalcieem@gmail.com>
2022-11-17 13:37:05 +0300

    Update Projects.js

--- v1/src/services/Projects.js
+++ v1/src/services/Projects.js
@@ -16,19 +16,22 @@ const insert = (data) => {
 
 const list = (where) => {  //* list'e sonucları attık. (where'i controllers/users->projectList için kullanıyoruz.)
     // boş find({}) yaparsak sonucları donuduruyor, yazarsak buluyor onu ve onu dönüdürüyor.
-    return Project.find(where ||  {}).populate({ // populate komutu ile dolduruyoruz. (models'de user_id'yi tanımlasaydık yapamazdık bunu.)
+    return Project.find(where ||  {}).populate({ // populate komutu ile dolduruyoruz. (models'de user_id'yi tanımlasaydık yapamazdık bunu çünkü user_id ile modelsdeki tanımlanan type ve ref'in veri tiplerini kullandık,
+                                                 // eğer kullanmasaydık, ref: user' yani referans aldıgı yer user. Bu demek oluyor ki user_id'nin user'dan populate olacağı ref:user sayesinde oluyor.
         path : "user_id",  // doldurmak istediğimiz 
-        select : "name email", // bu bilgiyi getir
+        select : "name email", // bu bilgileri getir
         // populate ile doldurarak yukardaki bilgileri bastırmıs oluyoruz.
 
     })  
 }
 
-const modify = (data, id) => { // burada update işlemi için kaydı bulcaz. bulma işlemi ise id ile kaydı bulma şeklinde olacak.
+ // burada update işlemi için kaydı bulcaz. Bulma işlemi ise id ile kaydı bulma şeklinde olacak.
+const modify = (data, id) => { //data: update olacak datadır.  id:id'den update olacak datayı bulacağız
     // return Project.findById(id).then(project => {  // project yazılmasının sebebi: id bulduk burdan pojecti getircez ki kayıt gelsin
     //     project.name  = data?.name // data içersindeki name'i al, project'in içersindeki name'e yaz
     //     return project.save();
     // });
+    
     // yukardakini tek satırda yapabiliriz: mongoose siteniden findByIdUpdate kullanımına göre yapabiliriz:
     return Project.findByIdAndUpdate(id, data, {new : true});
 };
