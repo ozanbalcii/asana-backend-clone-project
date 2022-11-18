31e8a10b8dafb18bdb1c7cd809fb5b590f37b3d4
ozan <ozanbalcieem@gmail.com>
2022-11-18 09:37:14 +0300

    Update index.js

--- v1/src/scripts/events/index.js
+++ v1/src/scripts/events/index.js
@@ -1,24 +1,25 @@
 const eventEmitter = require("./eventEmitter"); 
+const nodemailer = require("nodemailer");
 
 
 
 //email yollama işlemi burada olacak.
-module.exports = () => { //* fonk olarak export etmemizin sebebi : app.js'de events();'i kullanmamızdır.
-    eventEmitter.on("send_mail", (emailData) => { // eventi kullanıyoruz.(controllers'da) ( eventEmitter'de yeni event olustrduk, baska yapıdaki eventi kullanamıyoruz.)
-        console.log("evetn alındı", data);
-
+module.exports =  () => { //* fonk olarak export etmemizin sebebi : app.js'de events();'i kullanmamızdır.
+  
+    eventEmitter.on("send_mail", async(emailData) => { // eventi kullanıyoruz.(controllers'da) ( eventEmitter'de yeni event olustrduk, baska yapıdaki eventi kullanamıyoruz.)
+        // console.log("evetn alındı", data);
         // letli fonkları https://nodemailer.com/about/ 'dan kopyaladık ve değiştirdik.
+        
         let transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
-            secure: false, 
             auth: {
               user: process.env.EMAIL_USER, 
               pass: process.env.EMAIL_PASSWORD, 
             },
           });
 
-          let info =  transporter.sendMail({
+          let  info =  await transporter.sendMail({
             from: process.env.EMAIL_FROM,
             ...emailData, //email data'Nın geri kalan bilgisi burdan gelcek. Yanş şöyle: controllers'daki yapıdan belli bilgiler gelecek + buradan da geriye kalan bilgiler gelecek
           });
