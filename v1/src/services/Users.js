9943cae5d1ef40bcf53b3d9c0ce1aecc59e91c1c
ozan <ozanbalcieem@gmail.com>
2022-11-21 13:21:36 +0300

    Update Users.js

--- v1/src/services/Users.js
+++ v1/src/services/Users.js
@@ -19,10 +19,15 @@ const list = () => { // listeye datadaki bilgileri atcaz (find ile bulcaz ve atc
 };
 
 // şifre değiştirmek için, burda yapılmasının sebebi:1. olarak update ediyo yani DB'ye kaydedio, DB'ye kaydetme işlemi burda oluyordu.  2. neden ise: kayıt burada oluyor, email bilgisinden kullanıcıyı bulup şifre değişcez, mail bilgiside services'da çünkü db ye burda kaydediliyor.
-const modify = (where, data) => { // objeleri başka fonklarda almak için where. örnk: constrollers/user'da resetPassword'de
-    console.log(data);
+const modify = (where, Data) => { // objeleri başka fonklarda almak için where. örnk: constrollers/user'da resetPassword'de
     // findOneAndUpdate'e mongoose'dan bakılarak kullanıldı.(findOneAndUpdate: bir tane bulup güncelle, yan, şifre değiştir gibi bişi old için kullanılıyo.)
-    return User.findOneAndUpdate(where, data, {new: true}); // where, data, {new: true} --> objeyi aldık ve yeni obje olusturup döndürdük.
+    return User.findOneAndUpdate(where, Data, {new: true}); // where, data, {new: true} --> objeyi aldık ve yeni obje olusturup döndürdük.
+};
+
+const remove = (id) => { //id'den update olacak datayı bulacağız
+    //!  yukarda yapıp burada {new : true} yapmamazıın sebebi, yeni obje olusturulmaz remove'da (=
+    //* findByIdAndDelete yazılışına doc'dan baktık.(içine sadece id koyulacağını ordan öğrendik. )
+    return User.findByIdAndDelete(id); //? id gönderiyoruz ve o bize silinecek şeyi bulup siliyo
 };
 
 
@@ -31,4 +36,5 @@ module.exports = {
     list,
     loginUser,
     modify,
+    remove,
 };
