64dd6614332c2d41fbd1d2962968f1e30d7da4b3
ozan <ozanbalcieem@gmail.com>
2022-11-23 16:11:36 +0300

    Update Projects.js

--- v1/src/api-routes/Projects.js
+++ v1/src/api-routes/Projects.js
@@ -3,37 +3,14 @@ const validate= require('../middlewares/validate');
 const authenticate = require('../middlewares/authenticate');
 const schemas = require("../validations/Projects");
 const express = require('express');
-const { create, index, update, deleteProject } =  require("../controllers/Projects"); 
-const router = express.Router(); //! express'in router'ını kullanıyoruz.
-
-
- 
-
-// get request gelince create çalışıyor ardından creat'in konumu /controllers/Projects old için buraya gidiyor.
- router.route("/").get(authenticate, index);  
-  
- router
- .route("/")
-//validate'e schema lazım olan validate'nin kendi schema'sı = schemas.createValidation burada.
-//çünkü orada string olması, min:5 harf olması gibi koşullar var, onları validate ederse çalışacak.
- .post(authenticate, validate(schemas.createValidation), create);  // "/" gelince create'den once validate çalışacak.
-
- router.route("/:id").patch(authenticate, validate(schemas.updateValidation), update);
-
- router.route("/:id").delete(authenticate, deleteProject); //! hangi projeyi silmem gerektiğini söylemek için /:id yazıldı
+const { create, index, update } =  require("../controllers/Projects"); 
+const router = express.Router(); // express'in router'ını kullanıyoruz.
 
 
 
 
+router.route("/").get(authenticate, index);   
+router.route("/").post(authenticate, validate(schemas.createValidation), create); 
+router.route("/:id").patch(authenticate, validate(schemas.updateValidation), update); //! /:id -> projects id'ye göre yapmak için
 
 module.exports = router; // bunu direkt bu şekilde export edersek, app.js(genel)'de ProjectRouters.router yapmamıza gerek kalmıyor.(orda da  belirttim.)
-
-
-
- 
-
-
-
-
-
-
