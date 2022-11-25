4be23bbecdffb66ac653aced7123d7625e82db3d
ozan <ozanbalcieem@gmail.com>
2022-11-25 14:13:42 +0300

    Update app.js

--- v1/src/app.js
+++ v1/src/app.js
@@ -1,33 +1,37 @@
-//*mvp yapısına böldük(config, loaders.. vs gibi ama her şey bu dosya üstünden ilerlemektedir.)
 const express = require('express');
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const helmet = require('helmet');
 const config = require("./config");
 const loaders = require('./loaders');
-const events = require("./scripts/events");
+const errorHandler = require("./middlewares/errorHandler");
+const path = require("path");
+const {ProjectRoutes, UserRoutes, SectionRoutes, TasksRoutes } =  require('./api-routes'); 
 
-const {
-    ProjectRoutes, // bu api-router Project.js yi gösteriyor, bunun tanımını api-router index.js de yaptım
-    UserRoutes
- } =  require('./api-routes'); // {ProjectRoutes} --> ProjectRoutes'ın object olmasıdır.
 
 config();
 loaders();
-events();
-
-// const uuid = require("uuid");
-// console.log( uuid.v4());
 
 
+const app = express();
 
-const app = express(); //? express'i app değişkeninde kullanmak için.
-app.use(express.json()); //? body'deki bilgileri bize vermeye yarıyor
-app.use(helmet()); //? helmet'i kullanıyoruz.
-app.use(fileUpload());
+app.use(express.json());
+app.use(helmet()); 
+app.use(fileUpload()); 
+app.use("/uploads", express.static(path.join(__dirname, "./","uploads"))); 
 
-//! application ile suncuyu bind(bağlıyoruz) ediyoruz.
-app.listen(process.env.APP_PORT, () => { // port bilgisini .env'den alıyorum ve config() olmadan alamazdım çünkü env orada tanımlı.
-    console.log("Server started, PORT:" + process.env.APP_PORT);
-    app.use("/projects", ProjectRoutes); // reotuerı direkt export ettiğimiz için ProjectRouters.router yapmamıza gerek kalmıyor.
-    app.use("/users",  UserRoutes); 
-});   
+app.listen(process.env.APP_PORT, () => { 
+    console.log("Sunucu ayağa kalktı, PORT:" + (process.env.APP_PORT));
+    app.use("/projects", ProjectRoutes); 
+    app.use("/users", UserRoutes); 
+    app.use("/sections", SectionRoutes); 
+    app.use("/tasks", TasksRoutes); 
+
+    //! error handler:
+    app.use((req, res, next) => {      
+    const error = new Error("Aradağınız sayfa bulunmamaktadır.");
+    error.status = 404;
+    next(error);
+   });
+
+   app.use(errorHandler);  
+});
