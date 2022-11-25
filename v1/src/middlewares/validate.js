2a674af6bc6478a454b177ae901127722fc755cd
ozan <ozanbalcieem@gmail.com>
2022-11-25 14:14:30 +0300

    Update validate.js

--- v1/src/middlewares/validate.js
+++ v1/src/middlewares/validate.js
@@ -1,30 +1,20 @@
-//TÜM VALİDATİON İŞLEMLERİNİN BEYNİNİ BURDA OLUSTURDUK, HANGİ ŞEYLERİ KONTROL EDECEĞİNE VALİDATİON DOSYASI ALTINDA BAKIYORUZ, BURDA DA BEYNİ ÇAŞIŞYOR BAKILACAK ŞEYLERE GÖRE
-//* router'da geliyo validate'de bak eğer geçiyorsa devam geçmiyorsa error ver.
-// middleware'ler router seviyesindeler, bundan dolayı middleware'leri router dosyasında kullnacaz
-// validate'den geçermiyorsa durdurması lazım, bunun içinde araya girmeli, bundan dolayı middleware kullandık.
-// BODY'E gelen bilgiyi kontrol etmektedir. bu daha sonra kontrol ettiği yerler artacak. body kontrolünü joi ile yapıyoruz.
-
 const httpStatus = require("http-status");
-const validate = (schema) => (req, res, next) => { //*burada kurulan yapı, middleware'in genel sabit yapısıdır.
+const validate = (schema) => (req, res, next) => { 
 
-     // https://joi.dev/api/?v=17.6.1   buradan bakarak yazdım:
-     //burada validate'ten true cıkarsa value, çıkamaz ise false old için.
-    const { value, error }= schema.validate(req.body); // req.body'de data var ondan buraya yazdım
+     //* doc: https://joi.dev/api/?v=17.6.1  
+    const { value, error }= schema.validate(req.body); 
     if(error){
-        // alttaki kod sayesinde, error.details =[{message:""}, {message:""}] buna dönüşüyor: ["","",""] ve "aaa,bbb,ccc" haline geldi en son join sayesinde.
-        const errorMessage = error.details?.map((detail) => detail.message).join(", ") //* sadece error mesajlarını bana array içinde dön yaptık burda.
+        
+        const errorMessage = error.details?.map(detail => detail.message).join(", ") //? error.details =[{message:""}, {message:""}] --> ["","",""] --> "aaa,bbb,ccc" 
         res
         .status(httpStatus.BAD_REQUEST)
         .json({
-            error: errorMessage  //error message ile yolladık
+            error: errorMessage  
         });
         return;
     }
-    //returnleri koyunca sorun olmuyor ya da  araya else diyip de yapabilirdik
-    Object.assign(req, value); //*validate'den çıkan veri ile requestimi bind(bağlamak) edeceğiz.
-    return next();  //* validate'i sağlıyorsa if'e girmeyip buraya gelio ve next ile middleware'ı geçerek program çalışmaya başlıyor.
-
+    Object.assign(req, value); //! bind
+    return next();
 }; 
-
 
 module.exports = validate;
