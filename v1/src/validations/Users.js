1d5656043b671f371d12fa0d81b4a13feaaf4048
ozan <ozanbalcieem@gmail.com>
2022-11-25 14:15:47 +0300

    Update Users.js

--- v1/src/validations/Users.js
+++ v1/src/validations/Users.js
@@ -1,8 +1,8 @@
-const Joi = require ("joi"); //validation(doğrulama) için joi package'ını kullanıyoruz.
+const Joi = require ("joi"); 
 
 
 
-const createValidation = Joi.object({ //joi.object : joi'de obje üret
+const createValidation = Joi.object({ 
     full_name: Joi.string().required().min(3),
     password: Joi.string().required().min(8),
     email: Joi.string().email().required().min(8),
@@ -13,27 +13,24 @@ const loginValidation = Joi.object({
     email: Joi.string().email().required().min(8),
 });
 
-const resetPasswordValidation = Joi.object({   
-    email: Joi.string().email().required().min(8),
+const resetPasswordValidation = Joi.object({ 
+    email: Joi.string().email().required().min(8)
 });
 
-const updateValidation = Joi.object({   
-    full_name: Joi.string().min(3),
+const updateValidation = Joi.object({ 
     email: Joi.string().email().min(8),
+    full_name: Joi.string().min(3),
+    
+});
+const changePasswordValidation = Joi.object({ 
+    password: Joi.string().required().min(8),
 });
 
-const changePasswordValidation = Joi.object({   
-    password: Joi.string().required().min(8)
-});
 
-
-//* validation yaparken, bir işlem geldiğinde araya girip doğrulama yapması gerekmektedir.
-// bunun için middileware kullancağız.
-
-module.exports = {  // objede exports'ladık çünkü 1 den fazla validation yapılacak.
+module.exports = { 
     createValidation,
     loginValidation,
     resetPasswordValidation,
     updateValidation,
-    changePasswordValidation,    
-}
+    changePasswordValidation,
+};
