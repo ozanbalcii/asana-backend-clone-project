8e6aaa2552d20dedf81cef379c5c61a48e5dd2e9
ozan <ozanbalcieem@gmail.com>
2022-11-25 14:14:32 +0300

    Update Projects.js

--- v1/src/models/Projects.js
+++ v1/src/models/Projects.js
@@ -1,39 +1,25 @@
-//MODELS DOSYASINDA VERİ TABANINDAKİ OBJELERİN TANIMLARI BULUNACAKTIR.
-
 const Mongoose = require('mongoose');
-const logger = require("../scripts/logger/Projects"); // log basma işlemini burda yapcaz, ondan ekledik.
+const logger = require("../scripts/logger/Projects"); 
 
-
-//mongoose modelini olusturmamız için Schema Modellerimizi olusturuyoruz:
-const ProjectSchema = new Mongoose.Schema( // mongoose'a schema oluşturttuk.
+const ProjectSchema = new Mongoose.Schema( 
 {
     name: String,
     user_id : {
         type: Mongoose.Types.ObjectId,
-        ref : "user",
+        ref : "user"
+    }
     },
-    },
-    { timestamps: true, versionKey: false } //mongoda görmek istediğimiz ve istemediğimiz şeyleri ayarladık.
+    {timestamps: true, versionKey: false} 
 );
 
 
-
-//* schema burda oldugu için hook'u burada yazdık. ve bu dokumana gore yazdık: https://mongoosejs.com/docs/middleware.html#defining
-// hook yardımı ile loglama yapacağız.
-ProjectSchema.post("save" ,(doc)=>{      //"save" işlemi sonrasında loglama yapacağız.
-    logger.log({   // winston ile logger'ı bastırıyoruz.(logs diye dosya oluşturttuk winstona orada bastırdık.)
+//* doc: https://mongoosejs.com/docs/middleware.html#defining
+ProjectSchema.post("save" ,(doc)=>{      
+    logger.log({   
         level :"info",
         message : doc,
     });
 });
 
 
-
 module.exports = Mongoose.model("project", ProjectSchema);
-
-
-
-
-
-
-
