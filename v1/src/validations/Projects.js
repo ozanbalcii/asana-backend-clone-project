a2d933248675448a0c97a396181a5c2cfad20684
ozan <ozanbalcieem@gmail.com>
2022-11-25 14:15:33 +0300

    Update Projects.js

--- v1/src/validations/Projects.js
+++ v1/src/validations/Projects.js
@@ -1,26 +1,17 @@
-//create, update gibi işlemler için genel olarak validataion işlemleri burada gerçekleştirilecektir.
+const Joi = require ("joi"); 
 
-const Joi = require ("joi"); //validation(doğrulama) için joi package'ını kullanıyoruz.
-
-
-//*BURADA SCHEMA YAPTIK:çümkü joi dokumanına baktıgımızda böyle bir yapısı vardı.
-// name'in özelliklerini girdik, örneğin string, required ve min:5 olması gibi
-// bunlar validation'u sağlatan şeylerdir. Yani bu koşullar sağlanarak doğrulama yapılıyor.
-const createValidation = Joi.object({ //joi.object : joi'de obje üret
-    name: Joi.string().required().min(5),
-});
+const createValidation = Joi.object({ 
+    name: Joi.string().required().min(3),
+    project_id : Joi.string().min(8),
+});      
 
 const updateValidation = Joi.object({ 
-    name: Joi.string().required().min(5),
-});
-
-//* validation yaparken, bir işlem geldiğinde araya girip doğrulama yapması ve validate sağlanıyorsa devam sağlanmıyosa iptal etmesi gerekmekte.
-//* bunun için middileware kullancağız.
+    name: Joi.string().min(3),
+    project_id : Joi.string().min(8),
+}); 
 
 
-
-
-module.exports = {  // objede exports'ladık çünkü 1 den fazla validation yapılacak.
+module.exports = { 
     createValidation,
     updateValidation,
 };
